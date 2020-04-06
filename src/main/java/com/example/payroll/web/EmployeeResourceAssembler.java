7e09f1ab073c704598129cb428a45a2e651e6110
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-04-06 10:14:54 +0200

    EmployeeResourceAssembler was added

---
+++ src/main/java/com/example/payroll/web/EmployeeResourceAssembler.java
@@ -0,0 +1,20 @@
+package com.example.payroll.web;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.example.payroll.domain.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+class EmployeeResourceAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+	@Override
+	public EntityModel<Employee> toModel(Employee employee) {
+
+		return new EntityModel<>(employee,
+				linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	}
+}
